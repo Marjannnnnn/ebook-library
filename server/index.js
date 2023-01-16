25347dd0c9ea6fe8adabc422c9d2843520ba703b
Marjan02 <kevinzevalja@gmail.com>
2023-01-16 22:49:21 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const routes = require("./routes");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = process.env.PORT || 5000;
+const mongoURL = process.env.MONGO_URL;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/api", routes);
+
+app.get("/", (req, res) => {
+  res.send('ᗜ˰ᗜ');
+});
+
+mongoose
+  .connect(mongoURL, { dbName: "bookery-ecommerce-db" })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}...`);
+      console.log("Connected to database successfully!");
+    });
+  })
+  .catch((error) => {
+    console.log("Connection Failed!", error);
+  });
---
+++ server/routes/categories.router.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllCategoriesHandler,
+  getCategoryHandler,
+  postCategoryHandler,
+} = require("../controllers/categories.controller");
+
+router.route("/").get(getAllCategoriesHandler).post(postCategoryHandler);
+
+router.get("/:categoryId", getCategoryHandler);
+
+module.exports = router;
